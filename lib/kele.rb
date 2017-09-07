02e338a4679e35cd3d0e057778c76ca17d6db2c8
Isaac Orme <isorme1@gmail.com>
2017-09-07 14:22:17 -0700

    checkpoint-submission: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -43,4 +43,15 @@ class Kele
     response = self.class.post("https://www.bloc.io/api/v1/messages", body: message, headers: { "authorization" => @auth_token } )
   end
 
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    submission = {
+      "checkpoint_id": checkpoint_id,
+      "enrollment_id": @user_data["current_enrollment"]["id"],
+      "assignment_branch": assignment_branch,
+      "assignment_commit_link": assignment_commit_link,
+      "comment": comment
+    }
+    response = self.class.post("https://www.bloc.io/api/v1/checkpoint_submissions", body: submission, headers: { "authorization" => @auth_token } )
+  end
+
  end
